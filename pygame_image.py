a1e0797cfc9f92becb2a05b7671cbf4f6fd8c82a
c0a241589e <c0a241589e@edu.teu.ac.jp>
2025-04-15 16:10:26 +0900

    練習8背景反転

--- pygame_image.py
+++ pygame_image.py
@@ -12,16 +12,19 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png") #練習1
     kk_img = pg.transform.flip(kk_img, True, False) #練習2
+    bg2_img = pg.transform.flip(bg_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [400, 300])
+        x = tmr
+        screen.blit(bg_img, [-x, 0]) #6
+        screen.blit(bg2_img, [-x + 1600, 0]) 
+        screen.blit(kk_img, [300, 200]) #4
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200) #5
 
 
 if __name__ == "__main__":
