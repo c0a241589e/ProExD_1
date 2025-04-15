2524be950dd0e6535f23f2785e2335195ce8d3eb
c0a241589e <c0a241589e@edu.teu.ac.jp>
2025-04-15 16:37:14 +0900

    練習10コウカトン移動

--- pygame_image.py
+++ pygame_image.py
@@ -12,16 +12,28 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png") #練習1
     kk_img = pg.transform.flip(kk_img, True, False) #練習2
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
     bg2_img = pg.transform.flip(bg_img, True, False)
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        x = tmr
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0, 1)
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(1, 0)
+        x = tmr % 3200
         screen.blit(bg_img, [-x, 0]) #6
         screen.blit(bg2_img, [-x + 1600, 0]) 
-        screen.blit(kk_img, [300, 200]) #4
+        screen.blit(bg_img, [-x + 3200, 0]) 
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200) #5
